9c98581b6cdfd2aa6311a3f08d675ef9245e4336
Jakub Grochowski <grochowski.jakub96@gmail.com>
2021-10-08 12:50:31 +0200

    Add project files.

---
+++ FileReminderUdemy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileReminderUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
